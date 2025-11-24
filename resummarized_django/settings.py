22ddd28202976ef5fe405b4c7dc44aa739e87442
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-11-24 12:13:35 +0530

    Debug false

--- resummarized_django/settings.py
+++ resummarized_django/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG", default=0))
+DEBUG = os.getenv("DEBUG", default=0) == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
